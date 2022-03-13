d9361468107452052f9426a1a5904f81bb1babe5
Ramazan A <ramazan0504@mail.ru>
2022-03-13 17:51:55 +0300

    save other configs

--- app/src/main/java/in/oneplay/preferences/ServerPreferenceConfiguration.java
+++ app/src/main/java/in/oneplay/preferences/ServerPreferenceConfiguration.java
@@ -1,32 +1,49 @@
 package in.oneplay.preferences;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import in.oneplay.R;
 import in.oneplay.backend.ClientConfig;
 
 public class ServerPreferenceConfiguration {
-    static final String AUDIO_CONFIG_PREF_STRING = "list_audio_config";
+    private static final String FPS_PREF_STRING = "list_fps";
+    private static final String RESOLUTION_PREF_STRING = "list_resolution";
+    private static final String VIDEO_FORMAT_PREF_STRING = "video_format";
+    private static final String AUDIO_CONFIG_PREF_STRING = "list_audio_config";
 
-    private static void setAudioConfig(Context context, String audioType) {
+
+    private static void setBitrate(Context context, int bitrateKbps, SharedPreferences.Editor editor) {
+        if (bitrateKbps == 0) {
+            bitrateKbps = PreferenceConfiguration.getDefaultBitrate(context);
+        }
+        editor.putInt(PreferenceConfiguration.BITRATE_PREF_STRING, bitrateKbps);
+    }
+
+    private static void setAudioConfig(Context context, String audioType, SharedPreferences.Editor editor) {
         String[] audioNames = context.getResources().getStringArray(R.array.audio_config_names);
         String[] audioValues = context.getResources().getStringArray(R.array.audio_config_values);
 
         for (int i = 0; i < audioNames.length; i++) {
             if (audioNames[i].equals(audioType)) {
-                PreferenceManager
-                        .getDefaultSharedPreferences(context)
-                        .edit()
-                        .putString(AUDIO_CONFIG_PREF_STRING, audioValues[i])
-                        .apply();
+                editor.putString(AUDIO_CONFIG_PREF_STRING, audioValues[i]).apply();
                 return;
             }
         }
     }
 
     public static void savePreferences(Context context, ClientConfig config) {
-        // save audio config
-        setAudioConfig(context, config.getAudioType());
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+
+        editor.putString(FPS_PREF_STRING, Integer.toString(config.getGame_fps()));
+        editor.putString(VIDEO_FORMAT_PREF_STRING, config.getStreamCodec());
+        editor.putString(RESOLUTION_PREF_STRING, config.getResolution());
+
+        setBitrate(context, (int) config.getBitrateKbps(), editor);
+
+        setAudioConfig(context, config.getAudioType(), editor);
+
+        editor.apply();
     }
 }
