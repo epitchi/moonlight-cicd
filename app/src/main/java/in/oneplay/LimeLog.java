7c235f152abc205cc82da8c625cd3642c1e4952d
Sergey Strelchenko <serguncheouss@gmail.com>
2022-04-12 23:53:38 +0300

    Added stack trace to error log.

--- app/src/main/java/in/oneplay/LimeLog.java
+++ app/src/main/java/in/oneplay/LimeLog.java
@@ -22,13 +22,28 @@ public class LimeLog {
     }
     
     public static void severe(String msg) {
-        OneplayApi.getInstance().registerEvent(msg);
+        msg = msg + traceToString(new Throwable().getStackTrace());
+
         if (BuildConfig.DEBUG) {
-            LOGGER.severe(msg);
+        LOGGER.severe(msg);
+        } else {
+            OneplayApi.getInstance().registerEvent(msg);
         }
     }
     
     public static void setFileHandler(String fileName) throws IOException {
         LOGGER.addHandler(new FileHandler(fileName));
+    }
+
+    private static String traceToString(StackTraceElement[] trace) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < trace.length; i++) {
+            if (i != 0) {
+                sb.append("\n\tat ");
+                sb.append(trace[i]);
+            }
+        }
+
+        return sb.toString();
     }
 }
